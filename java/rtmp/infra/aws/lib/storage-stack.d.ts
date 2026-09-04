b2a4fe27b9a0d1d88fc6ae7f077d42c5cf49bce2
Tho Hoang Ngo <nhochamvui.love@gmail.com>
2026-09-04 17:39:05 +0700

    fix: add gitignore for aws cdk built files

--- java/rtmp/infra/aws/lib/storage-stack.d.ts
+++
@@ -1,8 +0,0 @@
-import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { Construct } from 'constructs';
-export interface HlsStorage {
-    readonly bucket: Bucket;
-    readonly distribution: Distribution;
-}
-export declare function createHlsStorage(scope: Construct): HlsStorage;
